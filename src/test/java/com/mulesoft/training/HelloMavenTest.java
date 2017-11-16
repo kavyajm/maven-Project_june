7f3aaf9b2790b539961d370f1b185aaa7f010451
kavyajm <33710601+kavyajm@users.noreply.github.com>
2017-11-15 23:07:47 -0600

    added flows

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,36 @@
+package com.mulesoft.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+    @Rule
+    public DynamicPort dynamicPort = new DynamicPort("http.port");
+	@Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+		System.out.print("\n\nJunit Test case #1: Http Dynamic Port ----> "+dynamicPort.getNumber()+"\n\n");
+        runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+	 @Test
+	 public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+	   MuleEvent event = runFlow("retrieveFlights");
+	   String contentType = event.getMessage().getOutboundProperty("Content-Type");
+	   assertEquals("application/json", contentType);
+	 }
+
+	
+	
+    @Override
+    protected String[] getConfigFiles() {
+        String[]files = {"maven-project.xml","global.xml"};
+        return files;
+    }
+
+}
